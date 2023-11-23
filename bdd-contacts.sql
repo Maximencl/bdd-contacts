e426af907f38c36befe3402a51d74724caec7c24
Maxime Nicolas <nico.maxime84@gmail.com>
2023-11-23 10:06:25 +0100

    Ajout de 10 enregistrements pour la TABLE contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -28,3 +28,16 @@ CREATE TABLE telephone (
     type TINYINT NOT NULL,
     FOREIGN KEY (id_contact) REFERENCES contacts(id) ON DELETE CASCADE
 );
+
+INSERT INTO 'contacts' ('nom', 'prenom', 'date_de_naissance', 'sexe', 'adresse', 'cp', 'ville', 'pays_iso_3') VALUES
+('Doe', 'John', '1990-01-15', 'Male', '123 Main St', '12345', 'City1', 'FRA'),
+('Smith', 'Jane', '1985-05-20', 'Female', '456 Oak St', '67890', 'City2', 'USA'),
+('Müller', 'Hans', '1982-11-08', 'Male', '789 Birch St', '23456', 'City3', 'GER'),
+('Rossi', 'Maria', '1995-03-25', 'Female', '101 Pine St', '78901', 'City4', 'ITA'),
+('Lopez', 'Carlos', '1988-09-10', 'Male', '202 Cedar St', '34567', 'City5', 'ESP'),
+('Smith', 'David', '1992-07-12', 'Male', '303 Maple St', '45678', 'City6', 'GBR'),
+('Yamada', 'Yuki', '1998-04-30', 'Female', '404 Elm St', '56789', 'City7', 'JPN'),
+('Johnson', 'Emily', '1980-12-03', 'Female', '505 Walnut St', '67890', 'City8', 'CAN'),
+('Turner', 'Michael', '1987-06-18', 'Male', '606 Pine St', '78901', 'City9', 'AUS'),
+('Silva', 'Ana', '1993-08-22', 'Female', '707 Oak St', '89012', 'City10', 'BRA');
+
