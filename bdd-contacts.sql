de72ce4c4407ff5edd2485a777b03fea59f8d7bf
Maxime Nicolas <nico.maxime84@gmail.com>
2023-11-23 09:52:21 +0100

    Création de la TABLE contacts

--- bdd-contacts.sql
+++ bdd-contacts.sql
@@ -1,2 +1,15 @@
 DROP DATABASE IF EXISTS exo_contacts;
 CREATE DATABASE exo_contacts CHARACTER SET `utf8` COLLATE = `utf8_general_ci`;
+
+CREATE TABLE contacts (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(70),
+    prenom VARCHAR(70),
+    date_de_naissance VARCHAR(70),
+    sexe VARCHAR(20),
+    adresse TEXT NOT NULL,
+    cp VARCHAR(10),
+    ville VARCHAR(70),
+    pays_iso_3 VARCHAR(3),
+    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3) ON DELETE SET NULL
+);
